378f7716c2b642a081cac012e05997b4a7b142cd
Shreeram N S <nsshreeram@gmail.com>
2025-07-07 11:02:40 +0530

    Anonymous object

---
+++ Java Core/src/AnonymousObject.java
@@ -0,0 +1,21 @@
+////object which is not assigned to any reference variable is called anonymous object if we use it multiple time it will create new object
+/*
+class A{
+    public A(){
+        System.out.println("Object Create");
+    }
+
+    public void show(){
+        System.out.println("A in");
+    }
+}
+public class AnonymousObject {
+    public static void main(String[] args) {
+        new A().show();  // anonymous Object
+        new A().show();  // anonymous Object
+
+
+    }
+}
+*/
+////        here Two Objects are create
